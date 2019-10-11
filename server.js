ba0e4ddc432696725d953737fed2196e075c33de
unknown <workdennis@yahoo.com>
2019-10-10 22:17:21 -0500

    added handlebars functionality, seems to work on surface

---
+++ server.js
@@ -0,0 +1,25 @@
+// Dependencies
+var express = require("express");
+var exphbs = require("express-handlebars");
+
+// Create an instance of the express app.
+var app = express();
+
+// Specify the port.
+var port = 3000;
+
+// Set Handlebars as the default templating engine.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+
+app.get("/", function(req, res) {
+  res.render("mainFood");
+});
+
+app.get("/desserts", function(req, res){
+	res.render("desserts");
+});
+
+app.listen(port);
